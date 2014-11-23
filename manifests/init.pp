e5038182fbb5fbd1c5bff4df08d20cd1682626fb
Phil Christensen <pchristensen@freelancersunion.org>
2014-11-23 17:40:44 -0500

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+# == Class: bootstrap
+#
+# Bootstrap utilities for EC2 and CloudFormation. A set of helper classes,
+# functions and facts to aid in creating userdata and cfn-init scripts.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default.
+#
+# === Examples
+#
+#  class { bootstrap:
+#    
+#  }
+#
+# === Authors
+#
+# Phil Christensen <phil@bubblehouse.org>
+#
+class bootstrap {
+
+
+}
