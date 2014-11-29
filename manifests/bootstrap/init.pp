bf53a687eacac7bf6f32cbcef0aef2770f1cb65c
Phil Christensen <pchristensen@freelancersunion.org>
2014-11-28 19:18:43 -0500

    move bootstrap essentials into its own module

---
+++ manifests/bootstrap/init.pp
@@ -0,0 +1,59 @@
+# == Class: aws
+#
+# Bootstrap utilities for EC2 and CloudFormation. A set of helper classes,
+# functions and facts to aid in creating userdata and cfn-init scripts.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default.
+#
+# === Examples
+#
+#  class { "aws":
+#    
+#  }
+#
+# === Authors
+#
+# Phil Christensen <phil@bubblehouse.org>
+#
+
+class aws::bootstrap
+(
+  $is_nat = $aws::bootstrap::params::is_nat,
+  $eni_id = $aws::bootstrap::params::eni_id,
+  $eip_allocation_id = $aws::bootstrap::params::eip_allocation_id,
+  $static_volume_size = $aws::bootstrap::params::static_volume_size,
+  $static_volume_encryption = $aws::bootstrap::params::static_volume_encryption,
+  $static_volume_tag = $aws::bootstrap::params::static_volume_tag
+) inherits aws::bootstrap::params {
+  include apt
+  include staging
+  
+  if("${ec2_instance_id}" == "") {
+    fail("Can't find EC2 instance ID fact, something is wrong.")
+  }
+
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0755'
+  }
+  
+  # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues
+  anchor { 'aws::bootstrap::begin': } ->
+  class { '::aws::bootstrap::install': } ->
+  class { '::aws::bootstrap::config': } ->
+  class { '::aws::bootstrap::resources': } ->
+  class { '::aws::bootstrap::attachments': } ->
+  class { '::aws::bootstrap::nat': } ->
+  anchor { 'aws::end': }
+}
