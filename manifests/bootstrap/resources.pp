84edbb5b41bbcca62be103375456093fa8a4e97a
Phil Christensen <pchristensen@freelancersunion.org>
2014-11-29 22:35:03 -0500

    try to get the volume creation working

--- manifests/bootstrap/resources.pp
+++ manifests/bootstrap/resources.pp
@@ -1,7 +1,7 @@
 class aws::bootstrap::resources {
   if($aws::bootstrap::static_volume_size > 0){
     $existing_volume_id = ec2_detect_volume($aws::bootstrap::static_volume_tag, $ec2_placement_availability_zone)
-    if($existing_volume_id == nil){
+    if($existing_volume_id == ""){
       $volume_id = ec2_create_volume($aws::bootstrap::static_volume_size, $aws::bootstrap::static_volume_encryption)
       ec2_create_tag($volume_id, "Name", $aws::bootstrap::static_volume_tag)
     }
