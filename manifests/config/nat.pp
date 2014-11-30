fdc5c410392ce203c58104237be64a796b139c54
Phil Christensen <pchristensen@freelancersunion.org>
2014-11-30 16:56:33 -0500

    try to see why this is failing, increase wait time

--- manifests/config/nat.pp
+++ manifests/config/nat.pp
@@ -9,22 +9,23 @@ class aws::config::nat {
   exec { "sysctl-ip-forward":
     command => "/sbin/sysctl -q -w net.ipv4.ip_forward=1",
     unless => "/usr/bin/test $(/sbin/sysctl -n net.ipv4.ip_forward) -eq 1",
-    notify => Exec['wait-5s']
+    notify => Exec['wait-10s']
   }->
 
   exec { "sysctl-send-redirects":
     command => "/sbin/sysctl -q -w net.ipv4.conf.${iface}.send_redirects=0",
     unless => "/usr/bin/test $(/sbin/sysctl -n net.ipv4.conf.${iface}.send_redirects) -eq 0",
-    notify => Exec['wait-5s']
+    notify => Exec['wait-10s']
   }->
 
   exec { "iptables-nat-rule":
     command => "/sbin/iptables -t nat -A POSTROUTING -o ${iface} -s ${range} -j MASQUERADE",
+    logoutput => on_failure,
     unless => "/sbin/iptables -t nat -C POSTROUTING -o ${iface} -s ${range} -j MASQUERADE"
   }
   
-  exec { 'wait-5s':
-    command => "/bin/sleep 5",
+  exec { 'wait-10s':
+    command => "/bin/sleep 10",
     refreshonly => true,
     before => Exec['iptables-nat-rule']
   }
