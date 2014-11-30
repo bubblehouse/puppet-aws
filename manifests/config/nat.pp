1bbf6a11ffeb937d52d794cef12d5d82f5d237e2
Phil Christensen <pchristensen@freelancersunion.org>
2014-11-30 02:09:25 -0500

    make sure nat stuff happens in order

--- manifests/config/nat.pp
+++ manifests/config/nat.pp
@@ -9,12 +9,12 @@ class aws::config::nat {
   exec { "sysctl-ip-forward":
     command => "/sbin/sysctl -q -w net.ipv4.ip_forward=1",
     unless => "/usr/bin/test $(/sbin/sysctl -n net.ipv4.ip_forward) -eq 1"
-  }
+  }->
 
   exec { "sysctl-send-redirects":
     command => "/sbin/sysctl -q -w net.ipv4.conf.${iface}.send_redirects=0",
     unless => "/usr/bin/test $(/sbin/sysctl -n net.ipv4.conf.${iface}.send_redirects) -eq 0"
-  }
+  }->
 
   exec { "iptables-nat-rule":
     command => "/sbin/iptables -t nat -A POSTROUTING -o ${iface} -s ${range} -j MASQUERADE",
