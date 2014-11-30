133b9dc62042c85037341c9989eb832ae86194f5
Phil Christensen <pchristensen@freelancersunion.org>
2014-11-30 02:16:34 -0500

    fix for hostname

--- manifests/bootstrap/config.pp
+++ manifests/bootstrap/config.pp
@@ -4,7 +4,7 @@ class aws::bootstrap::config inherits aws::bootstrap {
   ec2_create_tag($ec2_instance_id, "Name", $aws::bootstrap::instance_name)
   
   exec { "configure-hostname":
-    command => "/bin/hostname ${aws::bootstrap::instance_fqdn}"
+    command => "/bin/hostname ${aws::bootstrap::instance_fqdn}",
     unless => "/usr/bin/test \"${aws::bootstrap::instance_fqdn}\" == \"$(/bin/hostname)\""
   }
   
