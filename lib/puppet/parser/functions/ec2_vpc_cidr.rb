d6081fc37b0af2fc03f42b8bc82a1e960436a573
Phil Christensen <pchristensen@freelancersunion.org>
2014-11-30 13:10:29 -0500

    nat debug output

--- lib/puppet/parser/functions/ec2_vpc_cidr.rb
+++ lib/puppet/parser/functions/ec2_vpc_cidr.rb
@@ -1,7 +1,7 @@
 module Puppet::Parser::Functions
   newfunction(:ec2_vpc_cidr, :type => :rvalue) do |iface|
-    Puppet.send(:notice, "Finding MAC for #{iface}")
-    mac = lookupvar("macaddress_#{iface}") 
+    Puppet.send(:notice, "Finding MAC for #{iface[0]}")
+    mac = lookupvar("macaddress_#{iface[0]}") 
     Puppet.send(:notice, "Finding CIDR block for #{mac} with fact ec2_network_interfaces_macs_#{mac}_vpc_ipv4_cidr_block")
     lookupvar("ec2_network_interfaces_macs_#{mac}_vpc_ipv4_cidr_block")
   end
