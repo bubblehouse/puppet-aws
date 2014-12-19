89990a6ac837d440e59d014d803a7e331a9eefe3
Phil Christensen <pchristensen@freelancersunion.org>
2014-12-19 13:59:15 -0500

    create the puppetmaster cert

--- manifests/foreman/install.pp
+++ manifests/foreman/install.pp
@@ -1,8 +1,15 @@
 # Install all the dependencies needed for this module to function
 
 class aws::foreman::install inherits aws::foreman {
+  exec { "create-puppetmaster-cert":
+    command => "/usr/bin/puppet cert generate ${aws::bootstrap::instance_fqdn}",
+    creates => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}"
+  }
+
   class { '::foreman':
     environment => $aws::foreman::foreman_environment,
-    admin_password => $aws::foreman::admin_password
+    admin_password => $aws::foreman::admin_password,
+    server_ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}",
+    require => Exec['create-puppetmaster-cert']
   }
 }
