891b550e58cacbda9a448fb0df6dbb0c68d40466
Phil Christensen <pchristensen@freelancersunion.org>
2014-12-19 14:14:52 -0500

    added missing extension

--- manifests/foreman/install.pp
+++ manifests/foreman/install.pp
@@ -3,13 +3,13 @@
 class aws::foreman::install inherits aws::foreman {
   exec { "create-puppetmaster-cert":
     command => "/usr/bin/puppet cert generate ${aws::bootstrap::instance_fqdn}",
-    creates => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}"
+    creates => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem"
   }
 
   class { '::foreman':
     environment => $aws::foreman::foreman_environment,
     admin_password => $aws::foreman::admin_password,
-    server_ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}",
+    server_ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
     require => Exec['create-puppetmaster-cert']
   }
 }
