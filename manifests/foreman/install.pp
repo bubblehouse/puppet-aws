53ea00366ddff1f647f4a43ed6ba861677cd0c71
Phil Christensen <pchristensen@freelancersunion.org>
2014-12-23 15:38:36 -0500

    param fixes

--- manifests/foreman/install.pp
+++ manifests/foreman/install.pp
@@ -8,7 +8,6 @@ class aws::foreman::install inherits aws::foreman {
   }->
   
   class { '::foreman':
-    custom_repo => true,
     environment => $aws::foreman::foreman_environment,
     admin_password => $aws::foreman::admin_password,
     server_ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
