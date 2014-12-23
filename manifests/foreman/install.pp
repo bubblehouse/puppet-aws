0df4ca3ccd2270d27fd615c67d17b3e8719ae70f
Phil Christensen <pchristensen@freelancersunion.org>
2014-12-23 14:15:09 -0500

    try to remove puppet conf before installing foreman

--- manifests/foreman/install.pp
+++ manifests/foreman/install.pp
@@ -3,7 +3,13 @@
 class aws::foreman::install inherits aws::foreman {
   exec { "create-puppetmaster-cert":
     command => "/usr/bin/puppet cert generate ${aws::bootstrap::instance_fqdn}",
-    creates => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem"
+    creates => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
+    notify => Exec['rm-puppet-conf']
+  }
+
+  exec { "rm-puppet-conf":
+    command => "/bin/rm /etc/puppet/puppet.conf",
+    refreshonly => true
   }
 
   class { '::foreman':
@@ -11,6 +17,9 @@ class aws::foreman::install inherits aws::foreman {
     admin_password => $aws::foreman::admin_password,
     server_ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
     server_ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
-    require => Exec['create-puppetmaster-cert']
+    require => [
+      Exec['create-puppetmaster-cert'],
+      Exec['rm-puppet-conf']
+    ]
   }
 }
