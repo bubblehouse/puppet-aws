a18cfc023b0a10b9790242f06bf8bf868e78f2ae
Phil Christensen <pchristensen@freelancersunion.org>
2014-12-23 15:48:41 -0500

    no need for proxy dependencies

--- manifests/foreman/install.pp
+++ manifests/foreman/install.pp
@@ -1,16 +1,15 @@
 # Install all the dependencies needed for this module to function
 
 class aws::foreman::install inherits aws::foreman {
-  class { '::foreman_proxy':
-    custom_repo => true,
-    ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
-    ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
-  }->
-  
   class { '::foreman':
     environment => $aws::foreman::foreman_environment,
     admin_password => $aws::foreman::admin_password,
     server_ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
     server_ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
+  }
+
+  class { '::foreman_proxy':
+    ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
+    ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
   }
 }
