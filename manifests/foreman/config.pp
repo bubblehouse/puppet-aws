264ffa9a9dac77def2194a980d546d258a21b374
Phil Christensen <pchristensen@freelancersunion.org>
2014-12-23 11:30:27 -0500

    install project module on foreman server

--- manifests/foreman/config.pp
+++ manifests/foreman/config.pp
@@ -20,4 +20,64 @@ class aws::foreman::config inherits aws::foreman {
     mode => '0644',
     notify => Service['apache2']
   }
+  
+  file { "/etc/puppet/Puppetfile":
+    ensure => file,
+    content => join([
+      "# Install ${aws::foreman::base_module_vendor}-${aws::foreman::base_module_name}"
+      "forge 'https://forgeapi.puppetlabs.com'",
+      "",
+      "mod '${aws::foreman::base_module_vendor}-${aws::foreman::base_module_name}',",
+      "  :git => '${aws::foreman::base_module_repo}'"
+    ], "\n"),
+    owner => 'root', 
+    group => 'root', 
+    mode => '0644'
+  }
+
+  file { "/etc/puppet/Gemfile":
+    ensure => file,
+    content => join([
+      "# Dependencies for ${aws::foreman::base_module_vendor}-${aws::foreman::base_module_name}"
+      "source 'https://rubygems.org'",
+      "",
+      "gem 'librarian-puppet'",
+      "gem 'aws-sdk', '>=2.0.6.pre'"
+    ], "\n"),
+    owner => 'root', 
+    group => 'root', 
+    mode => '0644',
+    notify => Exec['clear-old-librarian']
+  }
+  
+  exec { "clear-old-librarian":
+    command => "/bin/rm -rf .tmp .librarian Puppetfile.lock Gemfile.lock",
+    cwd => "/etc/puppet",
+    refreshonly => true,
+    notify => Exec['librarian-init']
+  }
+
+  exec { "librarian-bundle-install":
+    command => "/usr/bin/bundle install",
+    cwd => "/etc/puppet",
+    creates => "/usr/local/bin/librarian-puppet",
+  }
+  
+  exec { "librarian-init":
+    command => "echo n | /usr/local/bin/librarian-puppet init",
+    cwd => "/etc/puppet",
+    environment => [
+      "USER=root",
+      "HOME=/root/"
+    ],
+    refreshonly => true,
+    require => Exec['librarian-bundle-install'],
+    notify => Exec['librarian-install']
+  }
+  
+  exec { "librarian-install":
+    command => "/usr/local/bin/librarian-puppet install",
+    cwd => "/etc/puppet",
+    refreshonly => true
+  }
 }
