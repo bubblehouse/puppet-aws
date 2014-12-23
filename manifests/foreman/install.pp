9e91267b42ad4f6c097406f88a7dc31f971f5efa
Phil Christensen <pchristensen@freelancersunion.org>
2014-12-23 10:46:27 -0500

    added missing key param

--- manifests/foreman/install.pp
+++ manifests/foreman/install.pp
@@ -9,6 +9,7 @@ class aws::foreman::install inherits aws::foreman {
   class { '::foreman':
     environment => $aws::foreman::foreman_environment,
     admin_password => $aws::foreman::admin_password,
+    server_ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
     server_ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
     require => Exec['create-puppetmaster-cert']
   }
