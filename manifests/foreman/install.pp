99652c1060ea3b101b370a89d6781629ea14c9df
Phil Christensen <pchristensen@freelancersunion.org>
2015-02-05 12:18:39 -0500

    fixes for foreman hostnames

--- manifests/foreman/install.pp
+++ manifests/foreman/install.pp
@@ -2,14 +2,19 @@
 
 class aws::foreman::install inherits aws::foreman {
   class { '::foreman':
+    servername => $aws::bootstrap::instance_fqdn,
+    lower_fqdn => downcase($aws::bootstrap::instance_fqdn)
     environment => $aws::foreman::foreman_environment,
     admin_password => $aws::foreman::admin_password,
-    server_ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
-    server_ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
+
   }
 
   class { '::foreman_proxy':
     ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
     ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
+    puppet_url => "https://${aws::bootstrap::instance_fqdn}:8140",
+    foreman_base_url => "https://${aws::bootstrap::instance_fqdn}",
+    registered_name => $aws::bootstrap::instance_fqdn,
+    registered_proxy_url => "https://${aws::bootstrap::instance_fqdn}:8443
   }
 }
