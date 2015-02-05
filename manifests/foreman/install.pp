714f3c141b1d58752a55c382ba52573665a3a8ed
Phil Christensen <pchristensen@freelancersunion.org>
2015-02-05 15:53:21 -0500

    include cert name, cleanup

--- manifests/foreman/install.pp
+++ manifests/foreman/install.pp
@@ -2,19 +2,20 @@
 
 class aws::foreman::install inherits aws::foreman {
   class { '::foreman':
+    admin_password => $aws::foreman::admin_password,
     servername => $aws::bootstrap::instance_fqdn,
     environment => $aws::foreman::foreman_environment,
-    admin_password => $aws::foreman::admin_password,
+    server_certname => $aws::bootstrap::instance_fqdn,
     server_ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
     server_ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem"
   }
 
   class { '::foreman_proxy':
-    ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
-    ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
+    registered_name => $aws::bootstrap::instance_fqdn,
     puppet_url => "https://${aws::bootstrap::instance_fqdn}:8140",
     foreman_base_url => "https://${aws::bootstrap::instance_fqdn}",
-    registered_name => $aws::bootstrap::instance_fqdn,
-    registered_proxy_url => "https://${aws::bootstrap::instance_fqdn}:8443"
+    registered_proxy_url => "https://${aws::bootstrap::instance_fqdn}:8443",
+    ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
+    ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem"
   }
 }
