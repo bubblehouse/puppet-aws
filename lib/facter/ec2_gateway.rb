1d16efcf8e79f1ab1fc97b1f15c226b4aaa53db8
Dan Rosenbloom <drosenbloom@logicworks.net>
2015-03-09 15:33:10 -0400

    adding ec2_gateway fact

---
+++ lib/facter/ec2_gateway.rb
@@ -0,0 +1,16 @@
+require 'json'
+require 'ipaddr'
+
+Facter.add('ec2_gateway') do
+        setcode do
+                gateways = {}
+                metadata = Facter.value(:ec2_metadata)
+                interfaces = metadata["network"]["interfaces"]["macs"]
+                macs = interfaces.keys
+                macs.each{|mac|
+                        gateways["eth#{interfaces[mac]["device-number"]}".to_s] = IPAddr.new(interfaces[mac]["subnet-ipv4-cidr-block"]).succ.to_s
+                }
+                gateways
+        end
+end
+
