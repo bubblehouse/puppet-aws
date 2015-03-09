66277037f2f50c0529dc87a0e4fd2e32db2c9ef7
Dan Rosenbloom <drosenbloom@logicworks.net>
2015-03-09 17:01:33 -0400

    adding error catching.

--- lib/facter/ec2_gateway.rb
+++ lib/facter/ec2_gateway.rb
@@ -1,16 +1,18 @@
-require 'json'
 require 'ipaddr'
 
 Facter.add('ec2_gateway') do
   setcode do
-    gateways = {}
-    metadata = Facter.value(:ec2_metadata)
-    interfaces = metadata["network"]["interfaces"]["macs"]
-    macs = interfaces.keys
-    macs.each{|mac|
-      gateways["eth#{interfaces[mac]["device-number"]}".to_s] = IPAddr.new(interfaces[mac]["subnet-ipv4-cidr-block"]).succ.to_s
-    }
-    gateways
+    begin
+      gateways = {}
+      metadata = Facter.value(:ec2_metadata)
+      interfaces = metadata["network"]["interfaces"]["macs"]
+      macs = interfaces.keys
+      macs.each{|mac|
+        gateways["eth#{interfaces[mac]["device-number"]}".to_s] = IPAddr.new(interfaces[mac]["subnet-ipv4-cidr-block"]).succ.to_s
+      }
+      gateways
+    rescue e
+      Facter::Core::Logging.warn("Failure in ec2_gateway fact: #{e}")
   end
 end
 
