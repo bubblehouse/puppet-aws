ef481269446a8920b74e79701fd0e56bac4463d0
Phil Christensen <pchristensen@freelancersunion.org>
2015-03-10 12:19:09 -0400

    ensure that the environment for the hub node is already created

--- manifests/foreman/install.pp
+++ manifests/foreman/install.pp
@@ -1,6 +1,14 @@
 # Install all the dependencies needed for this module to function
 
 class aws::foreman::install inherits aws::foreman {
+  file { ["/etc/puppet/environments/${aws::foreman::foreman_environment}",
+          "/etc/puppet/environments/${aws::foreman::foreman_environment}/manifests",
+          "/etc/puppet/environments/${aws::foreman::foreman_environment}/modules"]:
+    ensure => directory,
+    owner => 'puppet',
+    group => 'root'
+  }
+  
   class { '::foreman':
     admin_password => $aws::foreman::admin_password,
     servername => $aws::bootstrap::instance_fqdn,
@@ -8,7 +16,8 @@ class aws::foreman::install inherits aws::foreman {
     server_ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
     server_ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
     websockets_ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
-    websockets_ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem"
+    websockets_ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
+    require => File["/etc/puppet/environments/${aws::foreman::foreman_environment}/modules"]
   }
   
   class { '::foreman_proxy':
@@ -18,6 +27,8 @@ class aws::foreman::install inherits aws::foreman {
     foreman_base_url => "https://${aws::bootstrap::instance_fqdn}",
     registered_proxy_url => "https://${aws::bootstrap::instance_fqdn}:8443",
     ssl_key => "/var/lib/puppet/ssl/private_keys/${aws::bootstrap::instance_fqdn}.pem",
-    ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem"
+    ssl_cert => "/var/lib/puppet/ssl/certs/${aws::bootstrap::instance_fqdn}.pem",
+    require => File["/etc/puppet/environments/${aws::foreman::foreman_environment}/modules"]
   }
+  
 }
