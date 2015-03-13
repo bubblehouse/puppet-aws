69bec11d7bfad4d286975003201fff0f03bd4215
Dan Rosenbloom <drosenbloom@logicworks.net>
2015-03-13 15:09:35 -0400

    misunderstood what the api response would be, fixed.

--- lib/puppet/parser/functions/vpc_attach_r53_zone.rb
+++ lib/puppet/parser/functions/vpc_attach_r53_zone.rb
@@ -6,8 +6,13 @@ module Puppet::Parser::Functions
     region = Facter.value(:ec2_placement_availability_zone).chop
     r53 = Aws::Route53::Client.new(region:region)
     begin
-      zones = r53.list_hosted_zones_by_name(dns_name: r53_zone)
-      if (zones[:hosted_zones].count == 0)
+      zones = r53.list_hosted_zones_by_name(dns_name: r53_zone).to_hash.select{|zone|
+          zone[:config][:private_zone] and (
+              (zone[:name] == r53_zone) or
+              (zone[:name] == "#{r53_zone}."))
+      }
+
+      if (zones.count == 0)
         Puppet.send(:debug, "No zones with the DNS name of #{r53_zone}, creating one.")
         zone = {}
         zone[:name] = r53_zone
@@ -18,13 +23,13 @@ module Puppet::Parser::Functions
         Puppet.send(:debug, resp[:change_info].to_s)
         sleep(10)
         zones = r53.list_hosted_zones_by_name(dns_name: r53_zone)
-        if zones[:hosted_zones].count != 1
+        if zones.count != 1
           Puppet.send(:warn, "Zone still not created, moving on.")
         else
-          zone_id = zones[:hosted_zones][0][:id]
+          zone_id = zones[0][:id]
         end
-      elsif zones[:hosted_zones].count == 1
-        zone_id = zones[:hosted_zones][0][:id]
+      elsif zones.count == 1
+        zone_id = zones[0][:id]
         Puppet.send(:debug, "Located Route53 Zone with DNS name of #{r53_zone} and id #{zone_id}.")
       else
         Puppet.send(:debug, "More than one zone with the DNS name of #{r53_zone}, taking no action.")
@@ -32,7 +37,7 @@ module Puppet::Parser::Functions
 
       if zone_id
         begin
-          zone = r53.get_hosted_zone(id: zone_id)[:hosted_zones]
+          zone = r53.get_hosted_zone(id: zone_id).to_hash[:hosted_zones]
           if zone[:vp_cs].select{|vpc| vpc[:vpc_id] == vpc_id}.count == 0
             Puppet.send(:debug, "Route53 zone #{r53_zone} not currently associated with vpc #{vpc_id}, associating.")
             r53.associate_vpc_with_hosted_zone(hosted_zone_id: zone_id, vpc: {vpc_region: region, vpc_id: vpc_id}, comment: "Associated by puppet-aws on #{Time.now}")
