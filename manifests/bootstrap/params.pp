60333305d0006724bdee2f320b9e2b3a827b9f76
Phil Christensen <pchristensen@freelancersunion.org>
2015-04-07 11:57:10 -0400

    saner defaults

--- manifests/bootstrap/params.pp
+++ manifests/bootstrap/params.pp
@@ -1,7 +1,7 @@
 class aws::bootstrap::params {
   $route53_internal_zone = nil
-  $instance_name = "${::cfn_baseinstancetag}-${::ec2_instance_slug}"
-  $instance_fqdn = "${instance_name}.${::cfn_endpointzone}"
+  $instance_name = "${hostname}"
+  $instance_fqdn = "${fqdn}"
   $is_nat = false
   $nat_cidr_range = nil
   $eni_interface = 'eth1'
