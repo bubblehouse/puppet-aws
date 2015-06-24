d76c3013b91b6f2328b1978a1b7578494e9ad5c4
Phil Christensen <pchristensen@logicworks.net>
2015-06-24 16:53:02 -0400

    add missing commas

--- manifests/install/ecs.pp
+++ manifests/install/ecs.pp
@@ -9,14 +9,14 @@ class aws::install::ecs($cluster_name) {
     ports => ['51678', '51678'],
     expose => ['51678', '51678'],
     volumes => [
-      "/var/run/docker.sock:/var/run/docker.sock"
-      "/var/log/ecs/:/log"
+      "/var/run/docker.sock:/var/run/docker.sock",
+      "/var/log/ecs/:/log",
       "/var/lib/ecs/data:/data"
     ],
     env => [
-      "ECS_LOGFILE=/log/ecs-agent.log"
-      "ECS_LOGLEVEL=info"
-      "ECS_DATADIR=/data"
+      "ECS_LOGFILE=/log/ecs-agent.log",
+      "ECS_LOGLEVEL=info",
+      "ECS_DATADIR=/data",
       "ECS_CLUSTER=${cluster_name}"
     ]
   }
