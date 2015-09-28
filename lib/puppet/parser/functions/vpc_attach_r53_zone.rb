f425cfb884819a2b5e3dd206f7ab91a380a3bd18
Dan Rosenbloom <drosenbloom@logicworks.net>
2015-09-28 14:32:10 -0400

    Requirements

--- lib/puppet/parser/functions/vpc_attach_r53_zone.rb
+++ lib/puppet/parser/functions/vpc_attach_r53_zone.rb
@@ -2,6 +2,8 @@ require 'securerandom'
 
 module Puppet::Parser::Functions
   newfunction(:vpc_attach_r53_zone) do |args|
+    require 'aws-sdk'
+    
     vpc_id, r53_zone = *args
     region = Facter.value(:ec2_placement_availability_zone).chop
     r53 = Aws::Route53::Client.new(region:region)
